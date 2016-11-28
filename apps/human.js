4e815285e33fddcb7dd382feb40914f397596453
OmokaroFaith <faithekhorowa@gmail.com>
2016-11-28 19:46:09 +0100

    update assignment solutions

--- apps/human.js
+++ apps/human.js
@@ -8,3 +8,13 @@ function Human (name, gender, height, skin_color){
 Human.prototype.getage = function(yob) {
   return (new Date().getFullYear() - yob);
 };
+
+function Doctor(name, gender, height, skin_color, field){
+ 	Human.call(this, name, gender, height, skin_color);
+ 	this.field = field;
+}
+Doctor.prototype = Object.create(Human.prototype);
+Doctor.prototype.constructor = Doctor;
+Doctor.prototype.years_of_experience = function(start_year) {
+  return (new Date().getFullYear() - start_year);
+};
