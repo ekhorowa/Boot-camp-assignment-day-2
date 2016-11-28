8c66840be714f508cf020a087abf0df21c72ed1f
OmokaroFaith <faithekhorowa@gmail.com>
2016-11-28 15:03:09 +0100

    add app files

---
+++ apps/dataTypes.js
@@ -0,0 +1,40 @@
+module.exports= function(n){
+
+  if (typeof n === "string"){
+  return n.length;
+  }
+  if ( n === null ||n === undefined){
+    return ("no value");
+  }
+  if (typeof n === "boolean"){
+    return n;
+  }
+  if (typeof n === "number"){
+    if (n < 100 ){
+      return ("less than 100");
+    }
+    else if (n > 100){
+      return ("more than 100");
+    }
+    else{
+     return "equal to 100";
+
+   }
+  }
+    
+ if(n instanceof Array){
+    if (n.length < 3){
+      return undefined;
+    }
+    else {
+      return n[2];
+    }
+  }
+  
+  
+  if (typeof n === "function"){
+      return n(true);
+  }
+}
+
+
