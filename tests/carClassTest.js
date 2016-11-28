9e36f97f939327937e41bd431c6935f89aabe674
OmokaroFaith <faithekhorowa@gmail.com>
2016-11-28 17:26:57 +0100

    update car class test

--- tests/carClassTest.js
+++ tests/carClassTest.js
@@ -61,18 +61,3 @@ describe("Car Class: Create a car, make it drive", function() {
 
 });
 })();
-
-[3:33]  
-var chai = require('chai');
-var expect = chai.expect;
-var getPrime = require('../app/library.js');
-
-describe("Get Prime tests ", function() {
-
-it("should return [2, 3, 5, 7] for prime number from 0 to n", function() {
- expect(getPrime(10)).to.be.eql([2, 3, 5, 7]);
-});
-it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] for prime number from 0 to n", function() {
- expect(getPrime(30)).to.be.eql([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
-});
-})
